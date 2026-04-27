37e27410bc161220a3dd2a2ac050e20efd34fc6f
dulamsamsoni-alt <dulamsamsoni@gmail.com>
2026-04-27 17:42:21 +0300

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fundi Mteja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fundi Mteja'),
+      ),
+      body: const Center(
+        child: Text(
+          'Karibu Fundi Mteja!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
