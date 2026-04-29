426accb60abab88c49ed0146cdcb8efaf30ea704
dulamsamsoni-alt <dulamsamsoni@gmail.com>
2026-04-29 20:03:40 +0300

    Update main.dart
    Fix google login _ final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -68,8 +70,32 @@ final Future<FirebaseApp> _initialization = Firebase.initializeApp(
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return;
+      
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +108,7 @@ class LoginPage extends StatelessWidget {
             const Text('Karibu!', style: TextStyle(fontSize: 30)),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: signInWithGoogle, // HII NDIO IMEBADILIKA
               child: const Text('Login na Google'),
             ),
             ElevatedButton(
